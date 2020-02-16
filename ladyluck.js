c76cc2969776bba82ce9b5399ed726d4b868dfdc
TheStranjer <791672+TheStranjer@users.noreply.github.com>
2020-02-16 19:13:31 +0000

    Client starting

--- ladyluck.js
+++ ladyluck.js
@@ -7,3 +7,9 @@ const storage = new Matrix.SimpleFsStorageProvider(config.matrix.syncFile);
 const client = new Matrix.MatrixClient(config.matrix.homeserverUrl, config.matrix.accessToken, storage);
 
 Matrix.AutojoinRoomsMixin.setupOnClient(client);
+
+console.log("Client starting...");
+
+client.start().then(function () {
+	console.log("Client started!");
+});
