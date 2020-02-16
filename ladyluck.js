682a1d5e62ae6cf11f7b377f3bbae2210b54176c
TheStranjer <791672+TheStranjer@users.noreply.github.com>
2020-02-16 19:08:55 +0000

    First attempt to connect to client

--- ladyluck.js
+++ ladyluck.js
@@ -1,1 +1,9 @@
+// requirements
 const Matrix = require("matrix-bot-sdk");
+const fs     = require('fs');
+
+const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+const storage = new Matrix.SimpleFsStorageProvider(config.matrix.syncFile);
+const client = new Matrix.MatrixClient(config.matrix.homeserverUrl, config.matrix.accessToken, storage);
+
+Matrix.AutojoinRoomsMixin.setupOnClient(client);
