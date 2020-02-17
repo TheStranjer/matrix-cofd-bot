c61001c20febc6baaec595e772f7cc6e1058623a
TheStranjer <791672+TheStranjer@users.noreply.github.com>
2020-02-17 13:24:31 +0000

    Receive messages

--- ladyluck.js
+++ ladyluck.js
@@ -8,8 +8,34 @@ const client = new Matrix.MatrixClient(config.matrix.homeserverUrl, config.matri
 
 Matrix.AutojoinRoomsMixin.setupOnClient(client);
 
+client.on("room.message", handleCommand);
+
 console.log("Client starting...");
 
-client.start().then(function () {
-	console.log("Client started!");
+client.start().then(async function () {
+	console.log("Client started! Client user id: " + (await client.getUserId()));
 });
+
+async function shouldSkipEvent(ev) {
+	if (!ev.content) {
+		return true;
+	}
+
+	if (ev.content.msgtype !== "m.text") {
+		return true;
+	}
+
+	if (ev.sender === await client.getUserId()) {
+		return true;
+	}
+
+	return false;
+}
+
+async function handleCommand(roomId, ev) {
+	if (await shouldSkipEvent(ev)) {
+		return;
+	}
+
+	console.log(ev);
+}
